0f45af8fb759d28d13f9f25c0a3fadc26122be48
AxelBladelove <AxelBladelove@users.noreply.github.com>
2026-05-09 19:06:03 -0400

    intento_axel_2026-05-09T19-06-01_exercism_exit0

---
+++ Ejercicios/Hello World/hello_world.c
@@ -0,0 +1,58 @@
+﻿/*
+Estudio Socratico - instrucciones traducidas
+Ejercicio: Hello World
+
+# Hola Mundo
+
+Bienvenido a Hola Mundo en la Pista de C de Exercism.
+Si necesitas ayuda para ejecutar las pruebas o enviar tu código, revisa `HELP.md`.
+
+## Instrucciones
+
+El ejercicio introductorio clásico.
+Solo di "Hola, Mundo!".
+
+["Hola, Mundo!"][hello-world] es el programa tradicional para comenzar a programar en un nuevo lenguaje o entorno.
+
+Los objetivos son simples:
+
+- Modifica el código proporcionado para que produzca la cadena "Hola, Mundo!".
+- Ejecuta la suite de pruebas y asegúrate de que tenga éxito.
+- Envía tu solución y compruébala en el sitio web.
+
+Si todo va bien, estarás listo para obtener tu primer ejercicio real.
+
+[hello-world]: https://en.wikipedia.org/wiki/%22Hello,_world!%22_program
+
+## Fuente
+
+### Creado por
+
+- @JacobMikkelsen
+
+### Contribuido por
+
+- @bcc32
+- @benkelaar
+- @gea-migration
+- @h-3-0
+- @kytrinyx
+- @patricksjackson
+- @QLaille
+- @RealBarrettBrown
+- @ryanplusplus
+- @siebenschlaefer
+- @wolf99
+
+### Basado en
+
+Este es un ejercicio para introducir a los usuarios al uso de Exercism - https://en.wikipedia.org/wiki/%22Hello,_world!%22_program
+*/
+#include "hello_world.h"
+
+// Define the function itself.
+const char *hello(void)
+{
+   return "Hello, World!";
+}
+
